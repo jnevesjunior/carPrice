a6eec0d7a5b75c9a3195fb06a215f670cffe5a45
Jose Neves <contato@joseneves.com.br>
2022-06-14 18:11:06 -0300

    Initial Commit

---
+++ CarPrice/CarPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarPriceApp.swift
+//  CarPrice
+//
+//  Created by Jose Neves on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CarPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
