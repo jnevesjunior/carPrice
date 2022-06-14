b4c51489211854dd43ad8d6f77405eb61e58857d
Jose Neves <contato@joseneves.com.br>
2022-06-14 18:24:58 -0300

    Create CarDetails view

--- CarPrice/CarPriceApp.swift
+++ CarPrice/CarPriceApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CarPriceApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CarDetails()
         }
     }
 }
---
+++ CarPrice/Scenes/CarDetails.swift
@@ -0,0 +1,20 @@
+//
+//  CarDetails.swift
+//  CarPrice
+//
+//  Created by Jose Neves on 14/06/22.
+//
+
+import SwiftUI
+
+struct CarDetails: View {
+    var body: some View {
+        Text("Details")
+    }
+}
+
+struct CarDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        CarDetails()
+    }
+}
