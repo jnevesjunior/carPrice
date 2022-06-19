c0c3e5065c4cd012f75bf39a9cbc45872cd68ae5
Jose Neves <contato@joseneves.com.br>
2022-06-19 13:08:19 -0300

    Add the first sketch of details screen

---
+++ CarPrice/Components/DetailItem.swift
@@ -0,0 +1,50 @@
+//
+//  DetailItem.swift
+//  CarPrice
+//
+//  Created by Jose Neves on 18/06/22.
+//
+
+import SwiftUI
+
+struct DetailItem: View {
+    var title: String
+    var description: String
+    var alignment: Alignment = .center
+    
+    var body: some View {
+        VStack(alignment: alignment.horizontal) {
+            Text(title)
+                .font(.system(size: 18, weight: .bold))
+            Text(description)
+                .font(.system(size: 16))
+        }
+        .frame(maxWidth: .infinity, alignment: alignment)
+        .padding(.all)
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(.gray, lineWidth: 1)
+        )
+    }
+}
+
+struct DetailItem_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            DetailItem(title: "title",
+                       description: "desc wquewquye ueqyweuyqwt ",
+                       alignment: .leading)
+            .listRowSeparator(.hidden)
+            
+            DetailItem(title: "title",
+                       description: "desc",
+                       alignment: .center)
+            .listRowSeparator(.hidden)
+            
+            DetailItem(title: "title",
+                       description: "desc",
+                       alignment: .trailing)
+            .listRowSeparator(.hidden)
+        }
+    }
+}
--- CarPrice/Scenes/CarDetails.swift
+++ CarPrice/Scenes/CarDetails.swift
@@ -6,10 +6,81 @@
 //
 
 import SwiftUI
+import Charts
 
 struct CarDetails: View {
+    struct ChartData: Identifiable {
+        var id = UUID()
+        var date: String
+        var value: Double
+    }
+    
+    var data: [ChartData]
+    {[
+        ChartData(date: "Jan/20", value: 10000),
+        ChartData(date: "Jun/20", value: 11000),
+        ChartData(date: "Dec/20", value: 11500),
+        ChartData(date: "Jan/21", value: 20000),
+        ChartData(date: "Jun/21", value: 21000),
+        ChartData(date: "Jun/22", value: 34154.00),
+    ]}
+        
     var body: some View {
-        Text("Details")
+        NavigationView {
+            List {
+                VStack(alignment: .center) {
+                    Text("Detalhes")
+                        .font(.headline.bold())
+                    
+                    Chart(data) {
+                        LineMark(
+                            x: .value("Month", $0.date),
+                            y: .value("Price", $0.value)
+                        )
+                        PointMark(
+                            x: .value("Month", $0.date),
+                            y: .value("Price", $0.value)
+                        )
+                    }
+                    .frame(height: 250)
+                }
+                .listRowSeparator(.hidden)
+                
+                DetailItem(title: "Modelo",
+                           description: "Polo SPORTLINE I MOTION 1.6 T.Flex 5p",
+                           alignment: .leading)
+                .listRowSeparator(.hidden)
+
+                DetailItem(title: "Preço Médio",
+                           description: "R$ 34.154,00",
+                           alignment: .leading)
+                .listRowSeparator(.hidden)
+                
+                HStack() {
+                    DetailItem(title: "Marca",
+                               description: "VW - VolksWagen")
+                    
+                    Spacer(minLength: 20)
+                    
+                    DetailItem(title: "Ano Modelo",
+                               description: "2010 Gasolina")
+                }
+                .listRowSeparator(.hidden)
+                
+                HStack() {
+                    DetailItem(title: "Código Fipe",
+                               description: "005295-7")
+                    
+                    Spacer(minLength: 20)
+                    
+                    DetailItem(title: "Autenticação",
+                               description: "q7scrs6db9p")
+                }
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            
+        }
     }
 }
 
